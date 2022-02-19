92b4b18767886027f157cf59d518ccb802d75aad
lawruixi <lawruixi@gmail.com>
2022-02-19 10:37:36 +0000

    renders html now, and actually translates text (hopefully)

--- final_project/server.py
+++ final_project/server.py
@@ -6,19 +6,24 @@ app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
+    text_to_translate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to French"
+    french_text = translator.english_to_french(text_to_translate)
+    return french_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
+    text_to_translate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to English"
+    english_text = translator.french_to_english(textToTranslate)
+    return english_text
+
 
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
+    render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
